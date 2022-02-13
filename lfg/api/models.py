2faecba0cedf75d3cb9297494422725d6747080c
pepe_fabra_valverde <jofaval@outlook.com>
2022-02-13 22:07:36 +0100

    Create Profile model

--- lfg/api/models.py
+++ lfg/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 class Videogame(models.model):
     name = models.CharField(max_length=200)
@@ -16,4 +17,17 @@ class Party(models.model):
 
     def __str__(self) -> str:
         return self.name
+
+class Profile(models.model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    twitter = models.CharField(max_length=200)
+    instagram = models.CharField(max_length=200)
+    telegram = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    twitch = models.CharField(max_length=200)
+    epic_games = models.CharField(max_length=200)
+    steam = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.user.username
 
