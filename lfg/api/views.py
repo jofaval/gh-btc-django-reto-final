cd89857770f568791e8f96a3e6a255859460377b
pepe_fabra_valverde <jofaval@outlook.com>
2022-02-13 22:17:22 +0100

    Create Serializers Views

--- lfg/api/views.py
+++ lfg/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
 
 # Create your views here.
+class VideogameViewSet():
+    queryset = Videogame.objects.all().order_by('name')
+    serializer_class = VideogameSerializer
+
+class PartyViewSet():
+    queryset = Party.objects.all().order_by('name')
+    serializer_class = PartySerializer
+
+class ProfileViewSet():
+    queryset = Profile.objects.all().order_by('user.name')
+    serializer_class = ProfileSerializer
+
+class MessageViewSet():
+    queryset = Message.objects.all().order_by('creator')
+    serializer_class = MessageSerializer
