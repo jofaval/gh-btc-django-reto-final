2dced20fbacc17764ec5617e666809e5fabdd608
pepe_fabra_valverde <jofaval@outlook.com>
2022-02-14 00:47:18 +0100

    Only the created can edit/delete the message

--- lfg/api/models.py
+++ lfg/api/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.contrib import admin
 
-class Videogame(models.model):
+class LfgModel(admin.ModelAdmin):    
+    def has_add_permission(request) -> bool:
+        return True
+
+    def has_change_permission(request, obj=None) -> bool:
+        return False
+
+    def has_delete_permission(request, obj=None) -> bool:
+        return False
+
+class Videogame(LfgModel):
     name = models.CharField(max_length=200)
     genre = models.CharField(max_length=200)
     release_date = models.DateTimeField(default=timezone.now)
@@ -10,7 +21,7 @@ class Videogame(models.model):
     def __str__(self) -> str:
         return self.name
 
-class Party(models.model):
+class Party(LfgModel):
     name = models.CharField(max_length=200)
     videogame = models.ForeignKey(Videogame,on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
@@ -18,7 +29,7 @@ class Party(models.model):
     def __str__(self) -> str:
         return self.name
 
-class Profile(models.model):
+class Profile(LfgModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     twitter = models.CharField(max_length=200)
     instagram = models.CharField(max_length=200)
@@ -31,12 +42,22 @@ class Profile(models.model):
     def __str__(self) -> str:
         return self.user.username
 
-class Message(models.model):
+class Message(LfgModel):
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.CharField()
     deleted = models.BooleanField()
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(default=timezone.now)
+
+    def has_change_permission(request, obj=None) -> bool:
+        if not obj: return False
+
+        return settings.AUTH_USER_MODEL == obj.creator
+
+    def has_delete_permission(request, obj=None) -> bool:
+        if not obj: return False
+
+        return settings.AUTH_USER_MODEL == obj.creator
 
     def __str__(self) -> str:
         return self.content
