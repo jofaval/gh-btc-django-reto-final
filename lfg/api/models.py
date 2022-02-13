00ad18c1b59357cde0c37275c5ef42bba46a01d3
pepe_fabra_valverde <jofaval@outlook.com>
2022-02-13 22:04:58 +0100

    Created Party model

--- lfg/api/models.py
+++ lfg/api/models.py
@@ -9,4 +9,11 @@ class Videogame(models.model):
     def __str__(self) -> str:
         return self.name
 
-# Create your models here.
+class Party(models.model):
+    name = models.CharField(max_length=200)
+    videogame = models.ForeignKey(Videogame,on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.name
+
