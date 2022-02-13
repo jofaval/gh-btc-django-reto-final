45ec064442990cb5a6d2a54c1c13605affff5b0f
pepe_fabra_valverde <jofaval@outlook.com>
2022-02-13 22:03:48 +0100

    Created Videogame model

--- lfg/api/models.py
+++ lfg/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+
+class Videogame(models.model):
+    name = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    release_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.name
 
 # Create your models here.
